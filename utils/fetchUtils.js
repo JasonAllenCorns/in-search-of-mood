c6709879722d6a5bd46a27f786318bca96088ceb
JasonAllenCorns <jason.ongithub@gmail.com>
2023-02-11 21:57:13 -0800

    ci: cruft cleanup and added linting to pipeline

--- utils/fetchUtils.js
+++
@@ -1,9 +0,0 @@
-export const getter = async (url, session) => {
-  const res = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${session.user.access_token}`,
-    },
-  }).then((res) => res.json());
-
-  return res;
-};
